fdc72cd7a24bd50aa557de57cf7ced5fbd2fd935
Yuhe Ong <yuhe_ong@hotmail.com>
2022-07-06 22:16:48 +0800

    fix deploy errors

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -7,8 +7,13 @@ const Socials = ({ className }) => {
   return (
     <div className={`${className} flex`}>
       {myData.socials.map((social, index) => (
-        <a href={social.link} target="_blank">
-          <Button key={index}>
+        <a
+          href={social.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          key={index}
+        >
+          <Button>
             <span className="text-lg">{social.title}</span>
           </Button>
         </a>
